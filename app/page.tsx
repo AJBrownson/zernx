ebf81ee118af37d8b699d5afd41884d2eb207c7c
Anietie Brownson <anni.brownson@gmail.com>
2025-12-16 14:03:22 +0100

    feat: Add referrals page desktop and mobile completed

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,6 @@ import { FaShareFromSquare } from "react-icons/fa6";
 import { FiCopy, FiCheck } from "react-icons/fi";
 import { LuArrowRightToLine } from "react-icons/lu";
 
-
-
 export default function Home() {
   const inputRef = useRef<HTMLInputElement>(null);
   const [copied, setCopied] = useState(false);
@@ -43,8 +41,8 @@ export default function Home() {
 
       <section className="mt-4 md:mt-12 px-4 py-4">
         {/* Invite friends banner */}
-        <div className="flex justify-between items-center rounded-md bg-linear-to-b from-[#B1F128] via-[#00C65F] via-31% to-[#009288] w-full md:max-w-225 mx-auto">
-          <p className="ml-5 md:ml-10 font-bold font-manrope text-base md:text-2xl w-8 md:w-52.5 text-[#010501]">
+        <div className="relative overflow-hidden flex justify-between items-center rounded-md bg-linear-to-b from-[#B1F128] via-[#00C65F] via-31% to-[#009288] w-full py-4 md:max-w-225 mx-auto">
+          <p className="ml-5 md:ml-10 font-bold font-manrope text-base md:text-2xl w-28 md:w-52.5 text-[#010501]">
             Invite Friends. Unlock rewards.
           </p>
 
@@ -53,21 +51,22 @@ export default function Home() {
             alt=""
             width={200}
             height={200}
-            className="rounded-br-md"
+            className="absolute -right-16 md:right-0 bottom-0 h-full w-auto object-contain rounded-br-md"
           />
         </div>
 
         {/* referral rules div */}
         <div className="flex flex-col md:flex-row w-full md:gap-2 md:max-w-225 mx-auto">
-
           {/* referral code input */}
           <div className="bg-[#010501] mt-5 py-3 md:px-4 rounded-md">
-            <div className="relative mt-4 flex justify-between items-center py-2 px-3 rounded-xl bg-[#0b0f0e] border border-white/5 overflow-hidden md:w-[440px]">
-              {/* bottom glow */}
-              <div className="pointer-events-none absolute left-0.5 right-0.5 bottom-px h-0.5 rounded-full bg-[linear-gradient(to_top,rgba(32,245,105,0.95),rgba(32,245,105,0.35),rgba(32,245,105,0))]" />
+            <div
+              className="relative mt-4 flex justify-between items-center px-4 py-3 rounded-2xl bg-[#0b0f0e] border border-white/5 overflow-hidden md:w-110">
+              <div
+                className="pointer-events-none absolute inset-x-4 bottom-px h-0.5 rounded-full bg-[linear-gradient(to_right,rgba(177,241,40,0),rgba(177,241,40,0.95),rgba(177,241,40,0))]"
+              />
 
-              <span>
-                <p className="font-manrope font-medium text-xs md:text-sm">
+              <span className="relative z-10">
+                <p className="font-manrope font-medium text-xs md:text-sm text-white/90">
                   {userWalletAddress} has recently invited...
                 </p>
                 <p className="font-manrope font-semibold text-sm md:text-base text-[#B1F128]">
@@ -75,25 +74,11 @@ export default function Home() {
                 </p>
               </span>
 
-              <button className="text-[#B5B5B5] flex items-center gap-1 font-manrope font-medium text-xs md:text-sm">
+              <button
+                className="relative z-10 flex items-center gap-1 font-manrope font-medium text-xs md:text-sm text-[#B5B5B5]">
                 Position <LuArrowRightToLine size={12} />
               </button>
             </div>
-
-            {/* <div className="mt-4 flex justify-between items-center py-2 border border-green-500 rounded-md md:w-108.5">
-              <span className="">
-                <p className="font-manrope font-medium text-base">
-                  {userWalletAddress} has recently invited...
-                </p>
-                <p className="font-manrope font-semibold text-base md:text-lg text-[#B1F128]">
-                  {reward} USDT
-                </p>
-              </span>
-
-              <button className="text-[#B5B5B5] font-manrope font-medium text-sm md:text-base">
-                Position
-              </button>
-            </div> */}
 
             <div className="mt-2 md:mt-4">
               <p className="font-manrope text-[#B5B5B5] font-normal text-xs md:text-sm mb-2">
@@ -112,10 +97,10 @@ export default function Home() {
                     onClick={handleCopy}
                     className="absolute right-4 top-1/2 -translate-y-1/2 text-[#B1F128] cursor-pointer"
                   >
-                   {copied ? <FiCheck size={18} /> : <FiCopy size={18} />}
+                    {copied ? <FiCheck size={18} /> : <FiCopy size={18} />}
                   </button>
                 </div>
-                <button className="bg-[#0D3600] p-4 border-[1.5px] border-[#156200] rounded-full text-[#498F00] font-manrope text-sm cursor-pointer">
+                <button className="bg-[#0D3600] p-4 md:px-8 border-[1.5px] border-[#156200] rounded-full text-[#498F00] font-manrope text-sm cursor-pointer">
                   Confirm
                 </button>
               </span>
@@ -127,26 +112,25 @@ export default function Home() {
           </div>
 
           {/* referral rules */}
-          <div className="bg-transparent md:bg-[#010501] mt-3 md:mt-5 md:px-4 rounded-md">
+          <div className=" bg-[#010501] mt-3 md:mt-5 md:px-4 rounded-xl">
             <h2 className="font-manrope font-semibold text-sm md:text-base mb-3 md:mb-0">
               Referral rules
             </h2>
-          
-          <div className="bg-[#151313] md:bg-transparent py-2 px-2 rounded-xl">
-            <ol className="space-y-2 md:space-y-4 mt-2 md:mb-4">
-              {referralRules.map((rule, index) => (
-                <li
-                  key={index}
-                  className="flex items-start text-xs md:text-sm font-manrope font-medium"
-                >
-                  <span className="shrink-0 mr-2">{index + 1}.</span>
-                  <span className="">{rule}</span>
-                </li>
-              ))}
-            </ol>
+
+            <div className="bg-[#151313] md:bg-transparent py-2 px-2 rounded-xl">
+              <ol className="space-y-2 md:space-y-4 mt-2 md:mb-4">
+                {referralRules.map((rule, index) => (
+                  <li
+                    key={index}
+                    className="flex items-start text-xs md:text-sm font-manrope font-medium"
+                  >
+                    <span className="shrink-0 mr-2">{index + 1}.</span>
+                    <span className="">{rule}</span>
+                  </li>
+                ))}
+              </ol>
             </div>
           </div>
-
         </div>
       </section>
     </>
